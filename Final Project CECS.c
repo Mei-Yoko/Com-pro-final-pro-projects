6bf8755a4d8ffbc1df22a915b374aedfd997be25
Mei-Yoko <keyyy@Pro.local>
2025-09-19 22:00:01 +0700

    .

---
+++ Final Project CECS.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE 256       // ขนาดบรรทัดสูงสุด
+#define MAX_RECORDS 1000   // จำนวนข้อมูลสูงสุดที่เก็บได้
+#define FILE_NAME "license_requests.csv" // ชื่อไฟล์ CSV
