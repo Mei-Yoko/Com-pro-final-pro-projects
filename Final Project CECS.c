d90fdfdab4d608233c95a2781408a702c22fdbfe
Mei-Yoko <keypongphob@gmail.com>
2025-09-21 00:27:45 +0700

    upcoming

--- Final Project CECS.c
+++ Final Project CECS.c
@@ -5,3 +5,11 @@
 #define MAX_LINE 256       // ขนาดบรรทัดสูงสุด
 #define MAX_RECORDS 1000   // จำนวนข้อมูลสูงสุดที่เก็บได้
 #define FILE_NAME "license_requests.csv" // ชื่อไฟล์ CSV
+
+// โครงสร้างเก็บข้อมูลการขอใบอนุญาต
+typedef struct {
+    char requestID[20];     // หมายเลขคำขอ
+    char requestName[50];   // ชื่อผู้ขอ
+    char licenseType[50];   // ประเภทใบอนุญาต
+    char requestDate[20];   // วันที่ขอ
+    } LicenseRequest;
